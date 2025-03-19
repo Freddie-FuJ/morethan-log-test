1580e4d0e6886c35374ee8aca404b692ece1078e
Freddie-FuJ <168391734+Freddie-FuJ@users.noreply.github.com>
2025-03-19 18:20:28 +0800

    Update Scripts.tsx

--- src/layouts/RootLayout/Scripts.tsx
+++ src/layouts/RootLayout/Scripts.tsx
@@ -1,5 +1,5 @@
-import Script from "next/script"
-import { CONFIG } from "site.config"
+import Script from "next/script";
+import { CONFIG } from "site.config";
 
 const Scripts: React.FC = () => (
   <>
@@ -18,7 +18,26 @@ const Scripts: React.FC = () => (
         </Script>
       </>
     )}
-  </>
-)
+    {/* 引入Waline的样式文件 */}
+    <Script
+      id="waline-css"
+      strategy="beforeInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `<link rel="stylesheet" href="https://unpkg.com/@waline/client@v3/dist/waline.css" />`,
+      }}
+    />
+    {/* 引入Waline的脚本文件 */}
+    <Script type="module">
+      {`
+        import { init } from 'https://unpkg.com/@waline/client@v3/dist/waline.js';
 
-export default Scripts
+        // 初始化Waline的函数，稍后会在CommentBox组件中调用
+        window.initWaline = (options) => {
+          init(options);
+        };
+      `}
+    </Script>
+  </>
+);
+
+export default Scripts;
